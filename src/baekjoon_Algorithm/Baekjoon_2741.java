9853334d87ec48b95062b12f0652aa143d3b7d9f
Jo Sunghyeon <josh5734@naver.com>
2020-10-13 14:51:54 +0900

    Create Baekjoon_2741.java

---
+++ src/baekjoon_Algorithm/Baekjoon_2741.java
@@ -0,0 +1,16 @@
+package baekjoon_Algorithm;
+
+
+import java.util.Scanner;
+public class Baekjoon_2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
