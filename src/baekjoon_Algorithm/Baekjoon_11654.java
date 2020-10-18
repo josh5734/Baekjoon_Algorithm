d34e7f40ef1801b22a7403cb4526943a52087ed9
Jo Sunghyeon <josh5734@naver.com>
2020-10-18 20:06:20 +0900

    Create Baekjoon_11654.java

---
+++ src/baekjoon_Algorithm/Baekjoon_11654.java
@@ -0,0 +1,11 @@
+package baekjoon_Algorithm;
+import java.util.Scanner;
+public class Baekjoon_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println((int)a.charAt(0)); 
+	}
+
+}
