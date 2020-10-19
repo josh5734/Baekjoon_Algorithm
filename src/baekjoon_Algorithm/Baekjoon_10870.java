c8b8464b0513a2aab1eea3405999a613059da0ee
Jo Sunghyeon <josh5734@naver.com>
2020-10-19 19:56:58 +0900

    Create Baekjoon_10870.java

---
+++ src/baekjoon_Algorithm/Baekjoon_10870.java
@@ -0,0 +1,20 @@
+package baekjoon_Algorithm;
+import java.util.Scanner;
+public class Baekjoon_10870 {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fibo(n));
+
+
+	}
+
+	static int fibo(int n) {
+		if(n == 0) return 0;
+		else if(n == 1) return 1;
+		else{
+			return fibo(n-1) + fibo(n-2);
+		}
+	}
+}
