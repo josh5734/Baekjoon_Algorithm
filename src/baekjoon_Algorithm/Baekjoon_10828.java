607308a403f98ce523b178855ec376a54b6ae3f0
Jo Sunghyeon <josh5734@naver.com>
2020-10-19 19:56:55 +0900

    Create Baekjoon_10828.java

---
+++ src/baekjoon_Algorithm/Baekjoon_10828.java
@@ -0,0 +1,43 @@
+package baekjoon_Algorithm;
+
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Baekjoon_10828 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		int n = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			switch(st.nextToken()) {
+				case "push":
+					stack.push(Integer.parseInt(st.nextToken()));
+					break;
+				case "top":
+					System.out.println(stack.isEmpty() ? -1: stack.peek());
+					break;
+				case "size":
+					System.out.println(stack.size());
+					break;
+				case "empty":
+					System.out.println(stack.isEmpty() ? 1: 0);
+					break;
+				case "pop":
+					System.out.println(stack.isEmpty()? -1 : stack.pop());
+					break;
+			}
+		}
+		
+		
+	}
+
+}
+
