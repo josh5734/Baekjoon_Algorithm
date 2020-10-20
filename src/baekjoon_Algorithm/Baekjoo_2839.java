43ee3565a90db9cc38c7309e9d4113621fafa0b0
Jo Sunghyeon <josh5734@naver.com>
2020-10-20 13:17:49 +0900

    Create Baekjoo_2839.java

---
+++ src/baekjoon_Algorithm/Baekjoo_2839.java
@@ -0,0 +1,26 @@
+package baekjoon_Algorithm;
+
+import java.util.*;
+public class Baekjoo_2839 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		boolean available = false;
+		int max3 = n/3;
+		int max5 = n/5;
+		
+		for(int i = 0; i <= max3; i++){
+			for(int j = 0 ; j <= max5; j++) {
+				if(3 * i + 5 * j == n) {
+					System.out.println(i+j);
+					available = true;
+					return;
+				}
+			}
+		}
+		if(!available) System.out.println(-1);
+		
+	}
+
+}
