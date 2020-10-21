6070bbb8ef22344def0a830b74b4d52ef6e0b2bb
Jo Sunghyeon <josh5734@naver.com>
2020-10-21 19:12:44 +0900

    Create Baekjoon_10952.java

---
+++ src/baekjoon_Algorithm/Baekjoon_10952.java
@@ -0,0 +1,24 @@
+package baekjoon_Algorithm;
+
+import java.util.*;
+
+public class Baekjoon_10952 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int a = sc.nextInt();
+			int b= sc.nextInt();
+			if(a == 0 && b == 0) break;
+			else {
+				System.out.println(a+b);
+				String junk = sc.nextLine();
+				
+			}
+			
+		}
+		
+	}
+
+}
